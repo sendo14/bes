e7891cd0db9f374a33396927496a1514ebfca1a1
sendo14 <sendo@live.cn>
2017-01-09 21:55:37 +0800

    no message

--- 百思不得姐SEN/百思不得姐SEN/Classes/Essence/Controller/SENEssenceViewController.m
+++ 百思不得姐SEN/百思不得姐SEN/Classes/Essence/Controller/SENEssenceViewController.m
@@ -16,6 +16,11 @@
 
 @interface SENEssenceViewController() <UIScrollViewDelegate>
 
+@property (nonatomic, weak) UIView *titlesView;
+@property (nonatomic, weak) UIView *indicatorView;
+@property (nonatomic, weak) UIButton *selectedBtn;
+@property (nonatomic, weak) UIScrollView *contentView;
+
 @end
 
 @implementation SENEssenceViewController
@@ -30,11 +35,80 @@
 }
 
 - (void)setupContentView{
-
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    UIScrollView *contentView = [[UIScrollView alloc] init];
+    contentView.frame = self.view.bounds;
+    contentView.delegate = self;
+    contentView.pagingEnabled = YES;
+    [self.view insertSubview:contentView atIndex:0];
+    contentView.contentSize = CGSizeMake(contentView.width * self.childViewControllers.count, 0);
+    self.contentView = contentView;
+    
+    [self scrollViewDidEndScrollingAnimation:contentView];
 }
 
 - (void)setupTitlesView{
+    
+    // setup titlesView
+    UIView *titlesView = [[UIView alloc] init];
+    titlesView.backgroundColor = [UIColor colorWithRed:255/255 green:255/255 blue:255/255 alpha:0.7];
+    titlesView.width = self.view.width;
+    titlesView.height = 35;
+    titlesView.y = 64;
+    [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+    // setupIndicator
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    indicatorView.tag = -1;
+    self.indicatorView = indicatorView;
+    
+    // setupBtn
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
+    CGFloat width = titlesView.width / titles.count;
+    CGFloat height = titlesView.height;
+    for (NSInteger i = 0; i < titles.count; i++) {
+        UIButton *btn = [[UIButton alloc] init];
+        btn.width = width;
+        btn.height = height;
+        btn.x = i * width;
+        btn.tag = i;
+        [btn setTitle:titles[i] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+        btn.titleLabel.font = [UIFont systemFontOfSize:14];
+        [btn addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titlesView addSubview:btn];
+        
+        if (i == 0) {
+            btn.enabled = NO;
+            self.selectedBtn = btn;
+            
+            [btn.titleLabel sizeToFit];
+            self.indicatorView.width = btn.titleLabel.width;
+            self.indicatorView.centerX = btn.centerX;
+        }
+    }
+    [titlesView addSubview:indicatorView];// 指示器放在最后
+}
 
+- (void)titleClick:(UIButton *)btn{
+    self.selectedBtn.enabled = YES;
+    btn.enabled = NO;
+    self.selectedBtn = btn;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        self.indicatorView.width = btn.titleLabel.width;
+        self.indicatorView.centerX = btn.centerX;
+    }];
+    
+    CGPoint offset = self.contentView.contentOffset;
+    offset.x = self.contentView.width * btn.tag;
+    [self.contentView setContentOffset:offset animated:YES];
 }
 
 - (void)setupChildVC{
@@ -66,9 +140,30 @@
     SENLogFunc;
 }
 
+#pragma mark - <UIScrollViewDelegate>
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
+    NSInteger index = scrollView.contentOffset.x / scrollView.width;
+    
+    UITableViewController *vc = self.childViewControllers[index];
+    vc.view.x = scrollView.contentOffset.x;
+    vc.view.y = 0; // 默认20
+    vc.view.height = scrollView.height; // y上移之后整体上移了20，所以要重新设置高度
+    
+    CGFloat top = CGRectGetMaxY(self.titlesView.frame);
+    CGFloat bottom = self.tabBarController.tabBar.height;
+    vc.tableView.contentInset = UIEdgeInsetsMake(top, 0, bottom, 0);
+    vc.tableView.scrollIndicatorInsets = vc.tableView.contentInset;
+    
+    [scrollView addSubview:vc.view];
+    
+}
 
-
-
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    [self scrollViewDidEndScrollingAnimation:scrollView];
+    
+    NSInteger index = scrollView.contentOffset.x / scrollView.width;
+    [self titleClick:self.titlesView.subviews[index]];
+}
 
 
 
--- 百思不得姐SEN/百思不得姐SEN/Classes/Essence/Controller/SENWordViewController.m
+++ 百思不得姐SEN/百思不得姐SEN/Classes/Essence/Controller/SENWordViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SENWordViewController.h"
+#import <AFNetworking.h>
 
 @interface SENWordViewController ()
 
@@ -16,83 +17,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @"29";
+
+    
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php"
+                             parameters:params
+                               progress:nil
+                                success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                                    [responseObject writeToFile:@"/Users/apple/Documents/GitHub/duanzi.plist" atomically:YES];
+    }
+                                failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                                    SENLog(@"error");
+    }];
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 50;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    static NSString *ID = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell.backgroundColor = [UIColor purpleColor];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@----%zd", [self class], indexPath.row];
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
